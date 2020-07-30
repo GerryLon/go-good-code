37da16de407bc0bf697915163bbffabb0778ce8e
wanjilong <wanjilong@nova.net.cn>
2020-07-30 09:56:42 +0800

    add gomaxprocs test

---
+++ go/gomaxprocs/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	wg := &sync.WaitGroup{}
+	const N = 256
+	wg.Add(N)
+	ch := make(chan string, N)
+	for i := 0; i <N/2; i++ {
+		go func() {
+			defer wg.Done()
+			// fmt.Println("A:", i)
+			ch <- fmt.Sprintf("A: %d", i)
+		}()
+	}
+
+	for i := 0; i <N/2; i++ {
+		go func(i int) {
+			defer wg.Done()
+			// fmt.Println("B:", i)
+			ch <- fmt.Sprintf("B: %d", i)
+		}(i)
+	}
+
+	wg.Wait()
+	close(ch)
+
+	for s := range ch {
+		fmt.Println(s)
+	}
+}
