8b356f5f2068b090ce91d18f3f0d95ef183ae6e2
wanjilong <wanjilong@nova.net.cn>
2020-07-31 15:54:20 +0800

    add zero-copy implementation of string and bytes transformation

--- go/unsafe/main.go
+++ go/unsafe/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"unsafe"
+
+	"github.com/GerryLon/good-code/go/unsafe/unsafe_model"
 )
 
-type Programmer struct {
-	name string
-	age uint8
-}
-
 func unsafe_basic() {
+	type Programmer struct {
+		name string
+		age uint8
+	}
+
 	p := Programmer{"gerrylon", 18}
 	fmt.Println(p) // {gerrylon 18}
 
@@ -30,6 +33,50 @@ func unsafe_basic() {
 	fmt.Println(p) // {gl 20}
 }
 
+func unsafe_change_private_field() {
+	p := unsafe_model.Programmer{}
+	fmt.Println(p) // { 0}
+
+	// p.name = "gl" // Unresolved reference 'name'
+
+	name := (*string)(unsafe.Pointer(&p))
+	*name = "gl"
+	fmt.Println(p) // {gl 0}
+
+	// unsafe.Sizeof("") 即name字段的占的内存大小
+	age := (*uint8)(unsafe.Pointer(uintptr(unsafe.Pointer(&p)) + unsafe.Sizeof("")))
+	*age = 19
+	fmt.Println(p) // {gl 19}
+}
+
+func string2bytes(s string) []byte {
+	str := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	by := reflect.SliceHeader{
+		Data: str.Data,
+		Len:  str.Len,
+		Cap:  str.Len,
+	}
+	return *(*[]byte)(unsafe.Pointer(&by))
+}
+
+
+func bytes2string(bs []byte) string {
+	by := (*reflect.SliceHeader)(unsafe.Pointer(&bs))
+
+	str := reflect.StringHeader{
+		Data: by.Data,
+		Len:  by.Len,
+	}
+	return *(*string)(unsafe.Pointer(&str))
+}
+
+func stringAndBytesTransformationTest() {
+	s := "hello world"
+	fmt.Println(s == bytes2string(string2bytes(s)))
+}
+
 func main() {
-	
+	// unsafe_basic()
+	// unsafe_change_private_field()
+	stringAndBytesTransformationTest()
 }
--- go/unsafe/unsafe_model/model.go
+++ go/unsafe/unsafe_model/model.go
@@ -1,1 +1,6 @@
 package unsafe_model
+
+type Programmer struct {
+	name string
+	age uint8
+}
