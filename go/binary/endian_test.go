3add003b6e2b8b4c4ca51b07a568961faef73df2
gerrylon <gerrylon@163.com>
2020-08-22 11:08:26 +0800

    删除第三方依赖

--- go/binary/endian_test.go
+++ go/binary/endian_test.go
@@ -2,10 +2,10 @@ package binary
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestIsLittleEndian(t *testing.T) {
-	assert.True(t, IsLittleEndian())
+	if !IsLittleEndian() {
+		t.Errorf("not little endian")
+	}
 }
