ebfbc8ac11e0ab951396f2c398e074dd9ace4b77
gerrylon <gerrylon@163.com>
2020-09-02 09:24:20 +0800

    合并两个有序链表 递归解法

--- go/ds-problems/0002-合并两个有序链表/main.go
+++ go/ds-problems/0002-合并两个有序链表/main.go
@@ -46,6 +46,23 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	return dummyHead.Next
 }
 
+func solution2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = solution2(l1.Next, l2)
+		return l1
+	}
+
+	l2.Next = solution2(l2.Next, l1)
+	return l2
+}
+
 func main() {
 
 }
