7a0bb85b7f76846fdd1aa8e1ebcfe6362da39abf
gerrylon <gerrylon@163.com>
2020-09-05 10:32:52 +0800

    移动0

--- go/ds-problems/0018-移动0/main.go
+++ go/ds-problems/0018-移动0/main.go
@@ -25,29 +25,27 @@ import (
 )
 
 func moveZeroes(nums []int) {
-	// 当数组从后往前数， 一直是0时， 索引最小的0的索引
-	minIndex := len(nums)
-
-	// 从左到右扫描， 遇到一个0, 记此位置为i
-	// 将 i+1到 minIndex之间的数字前移([i+1, minIndex-1])， 然后将此0放到minIndex-1的位置
-	// 更新minIndex = minIndex - 1
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == 0 {
-			// 所有的0都移动完了，就退出
-			if i >= minIndex {
-				break
-			}
-			for j := i; j < minIndex-1; j++ {
-				nums[j] = nums[j+1]
-			}
-			nums[minIndex-1] = 0
-			minIndex--
+	startIndex := 0
+	n := len(nums)
+	var i int
+	for ; i < n; i++ {
+		if nums[i] != 0 {
+			nums[startIndex] = nums[i]
+			startIndex++
 		}
+	}
+
+	for i = startIndex; i < n; i++ {
+		nums[i] = 0
 	}
 }
 
 func main() {
 	arr := []int{0, 1, 0, 3, 12}
+	moveZeroes(arr)
+	fmt.Println(arr)
+
+	arr = []int{0, 0, 1}
 	moveZeroes(arr)
 	fmt.Println(arr)
 }
